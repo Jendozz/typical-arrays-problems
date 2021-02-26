a3b0cc45da10560f0c7ff60a8dd6de66d92f4bfd
Naumov Evgeniy <n_zhenek@mail.ru>
2021-02-26 19:01:56 +0400

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array != undefined && array.length != 0)
+  {
+    let result = Math.min(...array);
+    return result;
+  }
+  else return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array != undefined && array.length != 0)
+  {
+    let result = Math.max(...array);
+    return result;
+  }
+  else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array != undefined && array.length != 0)
+  {
+    let result = 0;
+      for (let i = 0; i < array.length; i++ )
+      {
+          result += array[i];
+      }
+      return result / array.length
+  }
+  else return 0;
 }
